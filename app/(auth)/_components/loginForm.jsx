56370fc98399f3a0dc3c5f5c6bbc25fb1ca353db
shadon016 <shadon016@gmail.com>
2025-06-13 17:53:21 +0600

    fixing auth issue

--- app/(auth)/_components/loginForm.jsx
+++ app/(auth)/_components/loginForm.jsx
@@ -1,7 +1,84 @@
-import React from "react";
+"use client";
+import Link from "next/link";
 
-const LoginForm = () => {
-  return <h1>hello</h1>;
-};
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { login } from "@/actions/index.js";
+
+function LoginForm() {
+  const [error, setError] = useState();
+  const router = useRouter();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const formData = new FormData(event.currentTarget);
+      const response = await login(formData);
+      if (!!response.error) {
+        setError(response.error);
+      } else {
+        router.push("/");
+        router.refresh();
+      }
+    } catch (error) {
+      setError({
+        message:
+          "ইমেইল অথবা পাসওয়ার্ড সঠিক নয়। অনুগ্রহ করে আবার চেষ্টা করুন।",
+      });
+    }
+  }
+  return (
+    <>
+      <div className="flex flex-col h-screen justify-center items-center">
+        {error && <p className="text-red-500">{error.message}</p>}
+        <form
+          className="max-w-xl mx-auto  p-6 rounded-lg shadow-md"
+          onSubmit={onSubmit}
+        >
+          <h2 className="text-xl font-bold mb-6 px-6 text-white">
+            একাউন্টে প্রবেশ করুন
+          </h2>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              ইমেইল
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              পাসওয়ার্ড
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            প্রবেশ করুন
+          </button>
+        </form>
+        <Link href="/signup">নতুন একাউন্ট তৈরি করুন</Link>
+      </div>
+    </>
+  );
+}
 
 export default LoginForm;
--- app/(auth)/_components/signupForm.jsx
+++ app/(auth)/_components/signupForm.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 function SignupForm() {
   const [error, setError] = useState("");
@@ -31,25 +32,106 @@ function SignupForm() {
       });
       res.status === 201 && router.push("/signin");
     } catch (error) {
-      setError(error.message);
+      setError("ইমেইল অথবা পাসওয়ার্ড সঠিক নয়। অনুগ্রহ করে আবার চেষ্টা করুন।");
     }
   }
+
   return (
     <>
-      <p>{error && error}</p>
-      <form onSubmit={onSubmit}>
-        <input type="text" placeholder="user name" name="username" />
-        <input type="email" placeholder="email" name="email" />
-        <input type="password" placeholder="password" name="password" />
-        <input type="phone" placeholder="phone" name="phone" />
-        <input type="adress" placeholder="address" name="address" />
-        <button
-          type="submit"
-          className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      <div className="flex flex-col h-screen justify-center items-center">
+        {error && <p className="text-red-500">{error.message}</p>}
+        <form
+          className="max-w-xl mx-auto  p-6 rounded-lg shadow-md"
+          onSubmit={onSubmit}
         >
-          sign in
-        </button>
-      </form>
+          <h2 className="text-xl font-bold mb-6 px-6 text-white">
+            আপনার তথ্য দিন
+          </h2>
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-700"
+            >
+              আপনার নাম
+            </label>
+            <input
+              type="username"
+              id="username"
+              name="username"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              ইমেইল
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              পাসওয়ার্ড
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="phone"
+              className="block text-sm font-medium text-gray-700"
+            >
+              ফোন নম্বর
+            </label>
+            <input
+              type="phone"
+              id="phone"
+              name="phone"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="address"
+              className="block text-sm font-medium text-gray-700"
+            >
+              ঠিকানা
+            </label>
+            <input
+              type="address"
+              id="address"
+              name="address"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            একাউন্ট তৈরি করুন
+          </button>
+        </form>
+        <Link href="/signin">লগইন করুন</Link>
+      </div>
     </>
   );
 }
